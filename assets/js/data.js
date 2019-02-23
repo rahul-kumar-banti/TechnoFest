053fae2f2a4f20d51c9bfabf3115de7cc6f5c607
rahulbanti36 <rahulbanti36@gmail.com>
2019-02-23 20:06:18 +0530

    second

---
+++ assets/js/data.js
@@ -0,0 +1,190 @@
+eventItem=[
+    {"eventheader":"BUSINESS QUIZ",
+"eventtitle":"<h6>Elimination Round</h6><ul><li>	There can only be 2 people in the team</li>"+
+"<li>Teams will be selected at the end of this round for the next round</li>"+
+"<li>This round shall have MCQs and time limit shall be 10 minutes</li>"+
+"<li>1 mark for every right answer and -0.5 for each wrong answer</li>"
+,
+"eventdetail":"<h6>Round 1</h6><hr>"+
+"<ul><li>Total questions 24 for 8 group (3 questions. Each group)"+
+"<li>10 marks for correct answer, and no negative marking for wrong answer"
++"<li>If a team cannot answer the question, then the question would be forwarded to the next team for full marks."
++"<li>Only 5 teams would be selected for the 2nd Round."
++"<li>Answering time is only 30 seconds</ul>"
++"<h6>Round 2 (Buzzer-Round)</h6><hr>"+
+"<ul><li>There will be 15 questions in this round"+
+"<li>No pass on if the answers are wrong"
++"<li>If a team cannot answer the question, then the question would be forwarded to the audience"
++"<li>Negative marking is there -10 for each wrong answer & +10 for right answer."
++"<li>Only 3 teams would be selected for the 3rd Round</ul>"
++"<h6>(Rapid-Fire-Round)</h6><hr>"+
+"<ul><li>1 minute for each team to answer 5 questions."+
+"<li>Each question carries 10 marks."
++"<li>5 marks will be deducted for each wrong answer."
++"<li>If a team cannot answer the question, they can ask for the next question."
++"<li>The question will not be forwarded to next team</ul>"
+},
+
+{
+    eventheader:"Debate",
+    eventtitle:" ",
+    eventdetail:"1.	Team members should be from the same Institute.<br>"+
+    "2.	A team must consist of 2 members. One student cannot be part of more than one team.<br>"+
+   " 3.	There is no limit to the number of team entries that can be received from a particular institute.<br>"+
+   " 4.	The Team or the Individual will be provided a topic on the spot and they have to present their views in favour or against the topic in the given time limit* (Time limits are bound to change as per the participation of teams)<br>"+
+    "5.	The submission will be accepted verbally. Use of abusive language or foul words is not allowed." 
+
+},{
+    eventheader:"AD-MAD SHOW",
+    eventtitle:"",
+    eventdetail:"1.	Team Size: Max 4 members<br>"+
+     "2.Time limit: 10 min for the presentation<br>"
+    +"3.Acting is compulsory. Teams may also use other modes of communication (Video, Power Point Presentation, Poster etc.)<br>"
+   +"4.Teams have to prepare advertisement on the given topic.<br>"
+   + "5.There is no entry fee.<br>"
+   +" 6.Use of abusive language is strictly prohibited.<br>"
+   +" 7.A presentation is to be made about your advertisement explaining the concept, the Product, the Marketing Mix & Cost of broadcasting it on Audio and Visual media. Laptops are to be brought by the participants."
+    
+}
+,{
+    eventheader:"CYBER GAME (Individual)",
+    eventtitle:"Need for Speed (Most Wanted)",
+    eventdetail:"1.	Participation fee of Rs 100 per individual<br>"+
+    "2.	Rules applicable same as the game<br>"+
+    "3.	No limit of number of participants from one institute"
+}
+,{
+    eventheader:" CYBER GAME (Counter Strike 1.6)",
+    eventtitle:"",
+    eventdetail:"1.	Participation fee of Rs 500 per team.<br>"+
+   " 2.	Rules applicable same as the game<br>"+
+    "3.	No limit of number of teams from one institute"
+}
+,{
+    eventheader:"SOLO DANCE",
+    eventtitle:"Team Size: Solo",
+    eventdetail:"<h6>Round(s): 1</h6>"+
+
+    "TIME LIMIT: 5-7 minutes (+2min setup and clearance time).<br>"+
+   " 1.	The track is to be arranged and provided to the organisers at least 30 minutes in advance to the event.<br>"+
+   " 2.	Vulgarity is strictly prohibited"
+}
+,{
+    eventheader:"Rangoli",
+    eventtitle:"",
+    eventdetail:"1.	There will be two members in a team.<br>"+
+    "2.	There is no theme for Rangoli<br>"+
+    "3.	Participants have to bring their Rangoli materials(color).<br>"+
+   " 4.	30 minutes will be given for preparing Rangoli<br>"
+    
+}
+,{
+    eventheader:"Face Painting",
+    eventtitle:"",
+    eventdetail:"1.	There will be one participant in a team and one volunteer will be allowed with the participant.<br>"+
+   " 2.	Colors will not be provided from the college. Participants have to bring their own materials.<br>"+
+"    3.	There will be no theme.<br>"+
+    "4.	30 minutes will be given to paint face."
+}
+,{
+    eventheader:"Web Designing",
+    eventtitle:"",
+    eventdetail:"1.	There will be one member in a team.<br>"+
+"    2.	30 minutes will be given to each participant to develop a web page.<br>"+
+  "  3.	Theme will be given to each participant to design interactive web page<br>"
+    
+}
+,{
+    eventheader:"Poster  Making",
+    eventtitle:"",
+    eventdetail:"1.	There will be one member in a team.<br>"+
+   " 2.	Participants will bring prepared poster with them.<br>"+
+   " 3.	There is no theme in poster<br>"+
+    "4.	2 minutes will be given to each participant to explain the poster.<br>"+
+  "  5.	Size of the poster should be standard chart paper"
+    
+}
+,{
+    eventheader:"Project Presentation",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"Heena",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"T-SHIRT PAINTING",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"NUKKAD NATAK",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"FASHION BASH",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"NUKKAD NATAK",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"GROUP DANCE",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"TREASURE HUNT",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"WAR OF BANDS",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"C-Programming",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"Solo Singing",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"Blind Coding",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"IT- Quiz",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"Java Programming Contest",
+    eventtitle:"",
+    eventdetail:""
+}
+,{
+    eventheader:"Fastest Finger First",
+    eventtitle:"",
+    eventdetail:"1.	There will be one participant in a team.<br>"+
+  "  2.	Every participant will have to give the answers of 20 questions (with 1 Mark) through"+
+   " 3.	Key pad of quiz kit  "+
+    "4.	Evaluation criteria based on-"+
+   " 5.	Number of correct answers"+
+   " 6.	Total Minimum Response time will have more priority (Minimum time)"+
+  "  7.	Result will automatically generated by the Computer system"
+  
+}
+
+];
---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".fa-hand-o-right").animate({color:"#6ab04c"},1000);
+    $(".popupwindow").hide();
+  $(".closePopUp").click(function (e) { 
+      e.preventDefault();
+      $(".popupwindow").hide();
+      
+  }); 
+  $(".popupwindow").dblclick(function (e) { 
+    e.preventDefault();
+    $(".popupwindow").hide();
+    
+});  
+
+ 
+  $(".list-item").click(function(){
+      var clickedevent=$(this).attr("id");
+       $(".popupcheader").html(eventItem[clickedevent-1].eventheader);
+      $(".popctitle").html("<span class='badge badge-success'>Register now &#8594;</span>"+eventItem[clickedevent-1].eventtitle);
+      $(".popupctext").html(eventItem[clickedevent-1].eventdetail);
+      
+    $(".popupwindow").show();
+  })
+  
+});
